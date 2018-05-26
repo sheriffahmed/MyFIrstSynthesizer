36d2a201e25a76eede44dfb7e8a05b7c78913c49
sheriffahmed <sheriff.v.ahmed@gmail.com>
2018-05-26 17:50:46 -0400

    Express now serves the sketch, index route blank due to drum sound errors

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile("./view/index.html")
+  console.log(path.resolve(__dirname, '../'))
+  res.sendFile("view/index.html", {"root": path.resolve(__dirname, '../') })
 });
 
 module.exports = router;
